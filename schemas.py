e838a0b64666292965bb09652a05fb0c35f3b60d
Rachel Kiesling <80165032+opheliasmoon@users.noreply.github.com>
2026-02-13 20:48:27 -0500

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,1 +1,34 @@
+from pydantic import BaseModel
+import datetime
+
+
+# -------- COURSE --------
+class CourseCreate(BaseModel):
+    CourseName: str
+    Credits: int
+    Department: str
+
+
+# -------- ENROLLMENT --------
+class EnrollmentCreate(BaseModel):
+    StudentID: int
+    CourseID: int
+
+
+# -------- GRADE --------
+class GradeCreate(BaseModel):
+    StudentID: int
+    CourseID: int
+    InstructorID: int
+    GradeValue: str
+
+
+# -------- SCHEDULE --------
+class ScheduleCreate(BaseModel):
+    CourseID: int
+    InstructorID: int
+    RoomNumber: str
+    DayOfWeek: str
+    StartTime: datetime.time
+    EndTime: datetime.time
 
