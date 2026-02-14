5ebc1b8a64c8c14b05a1b1cd6149ed066e030fba
Rachel Kiesling <80165032+opheliasmoon@users.noreply.github.com>
2026-02-13 20:40:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+from fastapi import FastAPI, Depends, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+import datetime
+
+import models
+from database import engine, SessionLocal
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
