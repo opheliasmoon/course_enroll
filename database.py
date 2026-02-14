1530059f408ee5bb3c3639be14d4eee04d7703b1
Rachel Kiesling <80165032+opheliasmoon@users.noreply.github.com>
2026-02-13 20:51:10 -0500

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 DATABASE_URL = "sqlite:///./course_enroll.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
-
